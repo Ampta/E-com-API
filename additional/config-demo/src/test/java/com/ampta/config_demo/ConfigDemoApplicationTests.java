7b9ec593e75832b6fef3a57a4b3568245f2b8772
nakanu <ampta77000@gmail.com>
2026-01-10 13:05:35 +0530

    working with different profiles

---
+++ additional/config-demo/src/test/java/com/ampta/config_demo/ConfigDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ampta.config_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
